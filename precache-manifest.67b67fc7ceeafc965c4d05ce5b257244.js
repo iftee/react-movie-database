287b3b31b362dd54472af8b4cafd09f4dcc013da
Iftakhar Hasan <hasan.km.iftakhar@gmail.com>
2020-02-29 22:08:16 +0600

    Updates

---
+++ precache-manifest.67b67fc7ceeafc965c4d05ce5b257244.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41e0d25715bc8c6eacbb91f3ca7a937",
+    "url": "/react-movie-database/index.html"
+  },
+  {
+    "revision": "5cc6320b69ddbe2ea1a3",
+    "url": "/react-movie-database/static/css/2.da0c8c99.chunk.css"
+  },
+  {
+    "revision": "745197947dd6b3ce7114",
+    "url": "/react-movie-database/static/css/main.6a0d33c7.chunk.css"
+  },
+  {
+    "revision": "5cc6320b69ddbe2ea1a3",
+    "url": "/react-movie-database/static/js/2.aaf78a9a.chunk.js"
+  },
+  {
+    "revision": "79bafa17aa4356457d069116d76dce6e",
+    "url": "/react-movie-database/static/js/2.aaf78a9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "745197947dd6b3ce7114",
+    "url": "/react-movie-database/static/js/main.ef88fd0e.chunk.js"
+  },
+  {
+    "revision": "d78ae831f730869f77de",
+    "url": "/react-movie-database/static/js/runtime-main.463d994a.js"
+  },
+  {
+    "revision": "27b65cb40d26f78354a4ac5abf87b2be",
+    "url": "/react-movie-database/static/media/PoweredByRectangle_Green.27b65cb4.svg"
+  },
+  {
+    "revision": "d0819fb2b5efe0777b9d496a81c24c2e",
+    "url": "/react-movie-database/static/media/jane-doe.d0819fb2.svg"
+  },
+  {
+    "revision": "4c05fee4ac9c9ad9a4526330d8c62c45",
+    "url": "/react-movie-database/static/media/john-doe.4c05fee4.svg"
+  },
+  {
+    "revision": "38f334bf1840f2b66a5d325d53188043",
+    "url": "/react-movie-database/static/media/no_poster.38f334bf.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-movie-database/precache-manifest.67b67fc7ceeafc965c4d05ce5b257244.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-movie-database/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
