37c9aa87223bccd5c8a2096716e878556c09583d
Iftakhar Hasan <hasan.km.iftakhar@gmail.com>
2020-04-14 13:32:49 +0600

    HTTPS for image links

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ const API_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = '###';
 
 // Images
-const IMAGE_BASE_URL ='http://image.tmdb.org/t/p/';
+const IMAGE_BASE_URL ='https://image.tmdb.org/t/p/';
 // Sizes: w300, w780, w1280, original
 const BACKDROP_SIZE = 'w1280'
 // Sizes: w92, w154, w185, w342, w500, w780, original
